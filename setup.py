ef8e8b5f15b945546d228618650fd6db890b1336
Andy Davidson <andy@nosignal.org>
2021-01-20 12:47:12 +0000

    changes to setup to allow this to systemwide install

--- setup.py
+++ setup.py
@@ -6,16 +6,16 @@ with open("README.md", "r") as fh:
 
 setup(
  name='flask-kafka',
- version='0.0.3',
+ version='0.0.3.2',
  packages=['flask_kafka'],
- install_requires=['kafka-python'],
+ install_requires=['confluent-kafka'],
  license = 'MIT',
- description = 'An easy to use kafka consumer that uses the kafka-python library, it runs concurently with your flask server',
+ description = 'An easy to use kafka consumer that uses the kafka-python library, it runs concurently with your flask server (fork to use confluent library)',
  author = 'Nimrod Kevin Maina',
  author_email = 'nimzy.maina@gmail.com',
  keywords = ['kafka','consumer','kafkaesque','flask','simple','consumer', 'flask style', 'decorator'],
  long_description=long_description,
  long_description_content_type="text/markdown",
- url="https://github.com/nimzymaina/flask_kafka",
+ url="https://github.com/andydavidson/flask_kafka",
  include_package_data=True,
 )
